0452f2d05744930f3e0088863ebfd6e83625a904
WHITE-BOY-100 <hp618247@gmail.com>
2026-05-12 02:07:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.post("/api/analyze", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on " + PORT);
+  console.log("🚀 Server running on " + PORT);
 });
